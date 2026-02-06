ca4b34f880db6ec5bb4ea7899f419ed0377ff14f
Sebassr19 <sebastian.rengifo_h@uao.edu.co>
2026-02-05 22:41:14 -0500

    aaaa
    aaaaa

--- My project/Assets/Game/Script/EjStudent/ControllerScene_3.cs
+++ My project/Assets/Game/Script/EjStudent/ControllerScene_3.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class Controller_Scene_3 : MonoBehaviour
 {
 
@@ -10,7 +11,7 @@ public class Controller_Scene_3 : MonoBehaviour
     public TMP_InputField tageS;
     public TMP_InputField tcourseS;
     public TMP_InputField tcodeS;
-    public TMP_Text panelText;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,36 +27,8 @@ public class Controller_Scene_3 : MonoBehaviour
 
     public void AddStudent()
     {
-        Student student = new Student(
-            tnameS.text,
-            tmailS.text,
-            int.Parse(tageS.text),
-            tcourseS.text,
-            tcodeS.text
-        );
-
-        list_students.Add(student);
-
-        ShowStudents();
-
-        void ShowStudents()
-        {
-            panelText.text = ""; // Limpia el texto antes de escribir
-
-            foreach (Student s in list_students)
-            {
-                panelText.text +=
-                    "Nombre: " + s.NameP +
-                    "\nCorreo: " + s.MailP +
-                    "\nEdad: " + s.AgeP +
-                    "\nCurso: " + s.CourseS +
-                    "\nCódigo: " + s.CodeS +
-                    "\n----------------------\n";
-            }
-        }
         Student student = new Student(tnameS.text, tmailS.text, int.Parse(tageS.text), tcourseS.text, tcodeS.text);
         list_students.Add(student);
         Debug.Log("Student Added: " + student.NameP + ", " + student.MailP + ", " + student.AgeP + ", " + student.CourseS + ", " + student.CodeS);
     }
-} 
-
+}
